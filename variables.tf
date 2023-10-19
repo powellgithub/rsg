b0384d0542c4365b7d422826aaa13d0bab8cfd73
powellgithub <117794662+powellgithub@users.noreply.github.com>
2023-10-19 11:27:52 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,8 +3,10 @@
 
 variable "prefix" {
   description = "The prefix used for all resources in this example"
+  address_prefixes       = ["10.0.0.0/24"]
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
+  default     = "eastus"
 }
